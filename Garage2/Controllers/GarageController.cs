f31ff01fdbe4d1b62d123ce62bb4234d2689850c
Peter Nilsson <incido@gmail.com>
2017-06-02 16:53:25 +0200

    Updated controller, translations etc.

--- Garage2/Controllers/GarageController.cs
+++ Garage2/Controllers/GarageController.cs
@@ -8,120 +8,150 @@ using System.Web;
 using System.Web.Mvc;
 using Garage2;
 using Garage2.Models;
+using System.Data.Entity.Infrastructure;
 
 namespace Garage2.Controllers
 {
-    public class GarageController : Controller
-    {
-        private VehiclesDB db = new VehiclesDB();
+	public class GarageController : Controller
+	{
+		private VehiclesDB db = new VehiclesDB();
 
-        // GET: Garage
-        public ActionResult Index()
-        {
-            return View(db.Vehicles.ToList());
-        }
+		// GET: Garage
+		public ActionResult Index()
+		{
+			return View(db.Vehicles.ToList());
+		}
 
-        // GET: Garage/Details/5
-        public ActionResult Details(string id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Vehicle vehicle = db.Vehicles.Find(id);
-            if (vehicle == null)
-            {
-                return HttpNotFound();
-            }
-            return View(vehicle);
-        }
+		// GET: Garage/Details/5
+		public ActionResult Details(string id)
+		{
+			if (id == null)
+			{
+				return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+			}
+			Vehicle vehicle = db.Vehicles.Find(id);
+			if (vehicle == null)
+			{
+				return HttpNotFound();
+			}
+			return View(vehicle);
+		}
 
-        public ActionResult Park()
-        {
-            return View();
-        }
+		public ActionResult Park()
+		{
+			Vehicle vehicleWithDefaults = new Vehicle();
+			return View(vehicleWithDefaults);
+		}
 
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Park([Bind(Include = "RegNr,Type,Brand,Model,Color,Wheels,EntryTime")] Vehicle vehicle)
-        {
-            if (ModelState.IsValid)
-            {
-				vehicle.EntryTime = DateTime.Now;
-				db.Vehicles.Add(vehicle);
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public ActionResult Park([Bind(Include = "RegNr,Type,Brand,Model,Color,Wheels")] Vehicle vehicle)
+		{
+			try
+			{
+				if (ModelState.IsValid)
+				{
+					// Set parking time
+					vehicle.EntryTime = DateTime.Now;
+					db.Vehicles.Add(vehicle);
+					db.SaveChanges();
+					return RedirectToAction("Index");
+				}
+			}
+			catch (DataException /* dex */)
+			{
+				//Log the error (uncomment dex variable name and add a line here to write a log.
+				//ModelState.AddModelError("", "Unable to save changes. Try again, and if the problem persists see your system administrator.");
+				ModelState.AddModelError("", "Kan inte spara ändringar. Försök igen och om problemet kvarstår kontakta din systemadministratör.");
+			}
+			return View(vehicle);
+		}
 
-            return View(vehicle);
-        }
+		public ActionResult Edit(string id)
+		{
+			if (id == null)
+			{
+				return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+			}
+			Vehicle vehicle = db.Vehicles.Find(id);
+			if (vehicle == null)
+			{
+				return HttpNotFound();
+			}
+			return View(vehicle);
+		}
 
-        // GET: Garage/Edit/5
-        public ActionResult Edit(string id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Vehicle vehicle = db.Vehicles.Find(id);
-            if (vehicle == null)
-            {
-                return HttpNotFound();
-            }
-            return View(vehicle);
-        }
+		[HttpPost, ActionName("Edit")]
+		[ValidateAntiForgeryToken]
+		public ActionResult EditPost(string id)
+		{
+			if (id == null)
+			{
+				return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+			}
+			var vehicleToUpdate = db.Vehicles.Find(id);
+			if (TryUpdateModel(vehicleToUpdate, "", new string[] { "RegNr", "Type", "Brand", "Model", "Wheels" }))
+			{
+				try
+				{
+					db.SaveChanges();
 
-        // POST: Garage/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "RegNr,Type,Brand,Model,Color,Wheels,EntryTime")] Vehicle vehicle)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(vehicle).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            return View(vehicle);
-        }
+					return RedirectToAction("Index");
+				}
+				catch (RetryLimitExceededException /* dex */)
+				{
+					//Log the error (uncomment dex variable name and add a line here to write a log.
+					//ModelState.AddModelError("", "Unable to save changes. Try again, and if the problem persists, see your system administrator.");
+					ModelState.AddModelError("", "Kan inte spara ändringar. Försök igen och om problemet kvarstår kontakta din systemadministratör.");
+				}
+			}
+			return View(vehicleToUpdate);
+		}
 
-        // GET: Garage/Delete/5
-        public ActionResult Delete(string id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Vehicle vehicle = db.Vehicles.Find(id);
-            if (vehicle == null)
-            {
-                return HttpNotFound();
-            }
-            return View(vehicle);
-        }
+		public ActionResult Checkout(string id, bool? saveChangesError = false)
+		{
+			if (id == null)
+			{
+				return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+			}
+			if (saveChangesError.GetValueOrDefault())
+			{
+				//ViewBag.ErrorMessage = "Delete failed. Try again, and if the problem persists see your system administrator.";
+				ViewBag.ErrorMessage = "Hämtning gick inte att genomföra. Försök igen och om problemet kvarstår kontakta din systemadministratör.";
+			}
+			Vehicle vehicle = db.Vehicles.Find(id);
+			if (vehicle == null)
+			{
+				return HttpNotFound();
+			}
+			return View(vehicle);
+		}
 
-        // POST: Garage/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(string id)
-        {
-            Vehicle vehicle = db.Vehicles.Find(id);
-            db.Vehicles.Remove(vehicle);
-            db.SaveChanges();
-            return RedirectToAction("Index");
-        }
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public ActionResult Checkout(string id)
+		{
+			try
+			{
+				Vehicle vehicle = db.Vehicles.Find(id);
+				db.Vehicles.Remove(vehicle);
+				db.SaveChanges();
+			}
+			catch (RetryLimitExceededException/* dex */)
+			{
+				//Log the error (uncomment dex variable name and add a line here to write a log.
+				return RedirectToAction("Delete", new { id = id, saveChangesError = true });
+			}
+			return RedirectToAction("Index");
+		}
 
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                db.Dispose();
-            }
-            base.Dispose(disposing);
-        }
-    }
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing)
+			{
+				db.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+	}
 }
--- Garage2/Models/Vehicle.cs
+++ Garage2/Models/Vehicle.cs
@@ -8,20 +8,33 @@ using System.Threading.Tasks;
 
 namespace Garage2
 {
-	public enum Vehicletypes { Airplane, Boat, Bus, Car, Motorcycle }
+//	public enum Vehicletypes { Airplane, Boat, Bus, Car, Motorcycle, Truck }
+	public enum Vehicletypes { Bil, Buss, Båt, Flygplan, Lastbil , Motorcykel }
 
 	public class Vehicle
 	{
+		[Display(Name ="Fordonstyp")]
 		public Vehicletypes Type { get; set; }
 		[Key]
 		[Required]
 		[StringLength(6, MinimumLength = 6)]
+		[Display(Name = "Registreringsnr")]
 		public string RegNr { get; set; }
+		[Display(Name = "Märke")]
 		public string Brand { get; set; }
+		[Display(Name = "Modell")]
 		public string Model { get; set; }
+		[Display(Name = "Färg")]
 		public string Color { get; set; }
+		[Display(Name = "Hjulantal")]
 		public int Wheels { get; set; }
+		[Display(Name = "Parkerat vid")]
 		public DateTime EntryTime { get; set; }
+
+		public Vehicle()
+		{
+			//Wheels = 0;
+		}
 
 		public virtual bool Match(string pattern)
 		{
