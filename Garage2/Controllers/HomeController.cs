5cd6d9bdf7959432b96ba03f50d238eaaa7302f7
Peter Nilsson <incido@gmail.com>
2017-06-08 01:12:11 +0200

    New Bootstrap theme Updated frontpage with viewmodel data

--- Garage2/Controllers/HomeController.cs
+++ Garage2/Controllers/HomeController.cs
@@ -3,14 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Garage2.Models;
 
 namespace Garage2.Controllers
 {
 	public class HomeController : Controller
 	{
+		private VehiclesDB db = new VehiclesDB();
+
 		public ActionResult Index()
 		{
-			return View();
+			var model = new HomeViewModel();
+			var vehicles = db.Vehicles;
+			model.Current = vehicles.Count();
+			if (model.Percent < 50)
+				model.ColorLevel = "success";
+			else if (model.Percent < 75)
+				model.ColorLevel = "warning";
+			else
+				model.ColorLevel = "danger";
+			return View(model);
 		}
 
 		public ActionResult About()
@@ -25,6 +37,15 @@ namespace Garage2.Controllers
 			ViewBag.Message = "Your contact page.";
 
 			return View();
+		}
+
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing)
+			{
+				db.Dispose();
+			}
+			base.Dispose(disposing);
 		}
 	}
 }
---
+++ Garage2/Models/HomeViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Garage2
+{
+	public class HomeViewModel
+	{
+		public int Current { get; set; }
+		public int Max { get; set; }
+		public int Percent {
+			get { return (int)Current * 100 / Max; }
+		}
+		public string ColorLevel { get; set; }
+
+		public HomeViewModel()
+		{
+			Max = 15;
+		}
+	}
+}
